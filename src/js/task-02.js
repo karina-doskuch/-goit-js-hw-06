14f4db7ddb091f34aaa4c24a6dbbabefa72da3c5
Karina Doskuch <karina.doskuch@gmail.com>
2022-10-05 19:55:01 +0300

    Task2 done.

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elementRefs = [];
+ingredients.forEach(ingredient => {
+  const liRef = document.createElement("li");
+  liRef.textContent = ingredient;
+  liRef.classList.add('item');
+  elementRefs.push(liRef);
+  
+});
+
+const ingredientsRef = document.querySelector('ul#ingredients');
+
+ingredientsRef.append(...elementRefs);
+console.log(ingredientsRef);
+

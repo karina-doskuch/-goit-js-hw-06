b41df8de7cf5f3579878015b62291920c45d857f
Karina Doskuch <karina.doskuch@gmail.com>
2022-10-07 18:01:27 +0300

    Task8 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector('body');
+const changeColor = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+changeColor.addEventListener('click', event => {
+  const color = getRandomHexColor();
+  bodyRef.style.backgroundColor = color;
+  colorSpan.textContent = color;
+
+});
+
+
+
+
+
+
+
+

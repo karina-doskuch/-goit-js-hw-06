41560e7fe7505815163d864847d0d4df3b389727
Karina Doskuch <karina.doskuch@gmail.com>
2022-10-07 14:04:46 +0300

    fixed bug

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,12 +1,15 @@
-// Задание 1
-
 const categoryItemsRef = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoryItemsRef.length}`);
 
-categoryItemsRef.forEach(categoryItemRef  => {
-    console.log('Category:', categoryItemRef.getElementsByTagName('h2')[0].textContent);
-    console.log('Elements:', categoryItemRef.getElementsByTagName('li').length);
+categoryItemsRef.forEach(item  => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements:${item.lastElementChild.children.length}`);
 });
 
 
 
+
+// categoryItemsRef.forEach(categoryItemRef  => {
+//     console.log('Category:', categoryItemRef.getElementsByTagName('h2')[0].textContent);
+//     console.log('Elements:', categoryItemRef.getElementsByTagName('li').length);
+// });

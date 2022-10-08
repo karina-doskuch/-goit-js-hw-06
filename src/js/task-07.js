ca2ceb1e4c6e3a493aca83905fb2cebb4877933a
Karina Doskuch <karina.doskuch@gmail.com>
2022-10-08 21:30:31 +0300

    fixed bug

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,6 +1,6 @@
 const inputChange = document.querySelector('#font-size-control')
 const spanText = document.querySelector('#text')
 
-inputChange.addEventListener('change', event => {
+inputChange.addEventListener('input', event => {
     spanText.style.fontSize = `${event.target.value }px`
 });
